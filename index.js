64ecec4d7015564614d67f600cbf7115e2313137
Christopher Valle <chrisbv@live.com>
2021-10-12 16:59:37 -0500

    commit tree 1f7c698e1c5b0d5f3868f54011929152484a2450

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return  blocks > 42 ? (blocks - 42) : (42 - blocks);
+}
+
+function distanceFromHqInFeet(feet){
+    return feet > 42 ? ((feet - 42) * 264) : ((42 - feet) * 264);
+}
+
+function distanceTravelledInFeet(dist1, dist2){
+    return dist1 > dist2 ? ((dist1 - dist2) * 264) : ((dist2 - dist1) * 264);
+}
+
+function calculatesFarePrice(start, destination){
+    let total = Math.max((start * 264), (destination * 264)) - Math.min((start * 264), (destination * 264));
+    if (total <= 400){
+        return 0;
+    } else if (total <= 2000){
+        return (((total - 400) * 2) / 100);
+    } else if (total <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
